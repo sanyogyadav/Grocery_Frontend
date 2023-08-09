24286e8702d9ee55f9b2043d53498a5cc3dc2b88
sanyogyadav <sanyogyadav52@gmail.com>
2023-08-09 17:12:12 +0530

    [SANYOG]-Footer and Main body part completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './component/signup/signup.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
   {
-    component: SignupComponent,
+    component: HomeComponent,
     path: ''
   },
+  {
+    component: SignupComponent,
+    path: 'user_signup'
+  },
+  {
+    component: LoginComponent,
+    path: 'user_login'
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,20 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { HttpClientModule } from '@angular/common/http';
-
+import { HeaderComponent } from './component/header/header.component';
+import {MatMenuModule} from '@angular/material/menu';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +42,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatRadioModule,
     MatCheckboxModule,
-    HttpClientModule
+    HttpClientModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
